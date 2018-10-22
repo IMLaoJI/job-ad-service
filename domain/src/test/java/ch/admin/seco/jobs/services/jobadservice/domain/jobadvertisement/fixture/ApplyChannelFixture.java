d778fd456c58812b95be21751e4a28a782a96deb
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-10-22 09:58:17 +0200

    DF-264 fixed testing error

--- domain/src/test/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/fixture/ApplyChannelFixture.java
+++ domain/src/test/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/fixture/ApplyChannelFixture.java
@@ -17,7 +17,7 @@ public class ApplyChannelFixture {
 
     public static Builder testApplyChannel() {
         return testApplyChannelEmpty()
-            .setRawPostAddress("mailAddress")
+            .setRawPostAddress("rawMailAddress")
             .setEmailAddress("emailAddress")
             .setPhoneNumber("phoneNumber")
             .setFormUrl("formUrl")
