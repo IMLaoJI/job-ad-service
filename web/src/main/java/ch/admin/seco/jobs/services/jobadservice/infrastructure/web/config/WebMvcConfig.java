dd9c7b69a1dfc63bb1968694e669b52573c720ac
Marton Biro <marton.biro@gmail.com>
2018-10-31 16:43:22 +0100

    DF-299: Add MDCInsertingServletFilter (#269)
    * DF-299: Add MDCInsertingServletFilter

--- web/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/config/WebMvcConfig.java
+++ web/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/config/WebMvcConfig.java
@@ -1,12 +1,19 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.web.config;
 
+import ch.qos.logback.classic.helpers.MDCInsertingServletFilter;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import java.util.EnumSet;
+
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements ServletContextInitializer, WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry formatterRegistry) {
@@ -15,4 +22,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         dateTimeFormatterRegistrar.registerFormatters(formatterRegistry);
     }
 
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        initMDCInserting(servletContext);
+    }
+
+    private void initMDCInserting(ServletContext servletContext) {
+        FilterRegistration.Dynamic mdcInsertingFilter = servletContext.addFilter("webappMDCInsertingFilter", new MDCInsertingServletFilter());
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
+
+        mdcInsertingFilter.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+        mdcInsertingFilter.setAsyncSupported(true);
+    }
 }
