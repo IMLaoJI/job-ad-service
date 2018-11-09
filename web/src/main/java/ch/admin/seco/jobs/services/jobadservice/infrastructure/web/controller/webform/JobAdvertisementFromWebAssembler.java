f6a13affa927771df3ce1834b56fc99e1f274712
Fabian Gotzen <fabian.gotzen@mimacom.com>
2018-11-09 11:54:49 +0100

    DF-352: Handle empty String Values in JobAdFromWebAssembler

--- web/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/controller/webform/JobAdvertisementFromWebAssembler.java
+++ web/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/controller/webform/JobAdvertisementFromWebAssembler.java
@@ -1,8 +1,13 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.web.controller.webform;
 
+import static org.springframework.util.StringUtils.hasText;
+import static org.springframework.util.StringUtils.trimWhitespace;
+
 import org.springframework.stereotype.Component;
 
 import ch.admin.seco.jobs.services.jobadservice.application.HtmlToMarkdownConverter;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.AddressDto;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.ApplyChannelDto;
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.create.CreateJobAdvertisementDto;
 
 @Component
@@ -19,7 +24,7 @@ public class JobAdvertisementFromWebAssembler {
                 .forEach(jobDescription -> jobDescription.setDescription(this.htmlToMarkdownConverter.convert(jobDescription.getDescription())));
         return new CreateJobAdvertisementDto()
                 .setReportToAvam(true)
-                .setApplyChannel(createJobAdvertisementFromWebDto.getApplyChannel())
+                .setApplyChannel(convertApplyChannel(createJobAdvertisementFromWebDto.getApplyChannel()))
                 .setCompany(createJobAdvertisementFromWebDto.getCompany())
                 .setContact(createJobAdvertisementFromWebDto.getContact())
                 .setEmployer(createJobAdvertisementFromWebDto.getEmployer())
@@ -33,7 +38,42 @@ public class JobAdvertisementFromWebAssembler {
                 .setExternalUrl(createJobAdvertisementFromWebDto.getExternalUrl())
                 .setPublicContact(createJobAdvertisementFromWebDto.getPublicContact())
                 .setLocation(createJobAdvertisementFromWebDto.getLocation())
-                .setOccupation(createJobAdvertisementFromWebDto.getOccupation())
-                ;
+                .setOccupation(createJobAdvertisementFromWebDto.getOccupation());
+    }
+
+    private ApplyChannelDto convertApplyChannel(ApplyChannelDto createApplyChannelDto) {
+        if (createApplyChannelDto == null) {
+            return null;
+        }
+        return new ApplyChannelDto()
+                .setRawPostAddress(trimOrNull(createApplyChannelDto.getRawPostAddress()))
+                .setPostAddress(convertPostAddress(createApplyChannelDto.getPostAddress()))
+                .setEmailAddress(trimOrNull(createApplyChannelDto.getEmailAddress()))
+                .setPhoneNumber(trimOrNull(createApplyChannelDto.getPhoneNumber()))
+                .setFormUrl(trimOrNull(createApplyChannelDto.getFormUrl()))
+                .setAdditionalInfo(trimOrNull(createApplyChannelDto.getAdditionalInfo()));
+    }
+
+    private AddressDto convertPostAddress(AddressDto createPostAddressDto) {
+        if (createPostAddressDto == null) {
+            return null;
+        }
+        if (!hasText(createPostAddressDto.getName())) {
+            return null;
+        }
+        return new AddressDto()
+                .setName(trimOrNull(createPostAddressDto.getName()))
+                .setStreet(trimOrNull(createPostAddressDto.getStreet()))
+                .setHouseNumber(trimOrNull(createPostAddressDto.getHouseNumber()))
+                .setPostalCode(trimOrNull(createPostAddressDto.getPostalCode()))
+                .setCity(trimOrNull(createPostAddressDto.getCity()))
+                .setPostOfficeBoxNumber(trimOrNull(createPostAddressDto.getPostOfficeBoxNumber()))
+                .setPostOfficeBoxPostalCode(trimOrNull(createPostAddressDto.getPostalCode()))
+                .setPostOfficeBoxCity(trimOrNull(createPostAddressDto.getPostOfficeBoxCity()))
+                .setCountryIsoCode(trimOrNull(createPostAddressDto.getCountryIsoCode()));
+    }
+
+    private String trimOrNull(String value) {
+        return hasText(value) ? trimWhitespace(value) : null;
     }
 }
