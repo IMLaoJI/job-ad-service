d7d6ded10b8411954e05a23ee49bcd0d0d4989d3
fago <fabian.gotzen@gmail.com>
2019-01-17 16:37:20 +0100

    DF-296 rename testclass

--- infrastructure-messagebroker/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/TestConfig.java
+++ infrastructure-messagebroker/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/TestConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
-public class TestApplicationConfig {
+public class TestConfig {
 
     @MockBean
     MailSenderService mailSenderService;
