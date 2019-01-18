556d468254e0ab95c3c89ead4b95fe70f7f37ff4
fago <fabian.gotzen@gmail.com>
2019-01-18 15:54:24 +0100

    DF-296 - add improved tests

--- infrastructure-messagebroker/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamServiceTest.java
+++ infrastructure-messagebroker/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamServiceTest.java
@@ -2,8 +2,12 @@ package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.av
 
 import ch.admin.seco.jobs.services.jobadservice.application.MailSenderData;
 import ch.admin.seco.jobs.services.jobadservice.application.MailSenderService;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationService;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.JobAdvertisementDto;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.update.CancellationDto;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.JobAdvertisement;
-import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.JobAdvertisementRepository;
+import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.JobAdvertisementId;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -13,19 +17,17 @@ import org.springframework.context.MessageSource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
-
 import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.fixture.JobAdvertisementFixture.testJobAdvertisement;
 import static ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.avam.AvamCancellationDtoFixture.testAvamCancellationDto;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class AvamServiceTest {
-
 
     @Autowired
     private AvamService avamService;
@@ -34,19 +36,23 @@ public class AvamServiceTest {
     private MailSenderService mailSenderService;
 
     @Autowired
-    private JobAdvertisementRepository jobAdvertisementRepository;
-
-    @Autowired
     private MessageSource messageSource;
 
-    @Test
-    public void sendEmailIfCancellationDtoNotFound() throws IOException {
-        // given
-        JobAdvertisement jobAdvertisement = testJobAdvertisement().build();
-        jobAdvertisementRepository.save(jobAdvertisement);
+    @Autowired
+    private JobAdvertisementApplicationService jobAdvertisementApplicationService;
 
+    @Before
+    public void setUp() {
+        JobAdvertisement jobAdvertisement = testJobAdvertisement().build();
+        JobAdvertisementDto jobAdvertisementDto = JobAdvertisementDto.toDto(jobAdvertisement);
+        when(jobAdvertisementApplicationService.findByStellennummerEgov(any())).thenReturn(jobAdvertisementDto);
+    }
+
+    @Test
+    public void sendEmailIfCancellationDtoNotFound() {
+        // given
         AvamCancellationDto cancellationDto = testAvamCancellationDto();
-        cancellationDto.setStellennummerAvam("NOTFOUND").setStellennummerEgov("NOTFOUND");
+
         // when
         avamService.handleCancelAction(cancellationDto);
 
@@ -58,7 +64,18 @@ public class AvamServiceTest {
         MailSenderData mailSenderData = mailSenderDataArgumentCaptor.getValue();
         assertThat(mailSenderData).isNotNull();
         assertThat(mailSenderData.getTemplateVariables()).isNotEmpty();
+        assertThat(mailSenderData.getTemplateName()).isEqualToIgnoringCase("JobAdCancelledMail_multilingual.html");
     }
 
+    @Test
+    public void dontSendCancellationEmailDirectlyIfFound() {
+        // given
+        AvamCancellationDto avamCancellationDto = testAvamCancellationDto();
 
+        // when
+        avamService.handleCancelAction(avamCancellationDto);
+
+        // then
+        verify(jobAdvertisementApplicationService, times(1)).cancel(any(JobAdvertisementId.class), any(CancellationDto.class), isNull());
+    }
 }
