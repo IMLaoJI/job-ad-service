e297aa5009a69d2b4718c665969cbb4752f15d7f
Patrick Dobler <patrick.dobler@gmail.com>
2019-03-19 09:39:39 +0100

    reverted

--- infrastructure-database/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/database/eventstore/StoredEventRepository.java
+++ infrastructure-database/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/database/eventstore/StoredEventRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface StoredEventRepository extends JpaRepository<StoredEvent, String> {
 
     @Query("select e from StoredEvent e where e.aggregateId = :aggregateId and e.aggregateType = :aggregateType order by e.registrationTime desc")
