65089bf459b1a4321814835c771111ec771125d7
Patrick Dobler <patrick.dobler@gmail.com>
2019-04-01 20:13:15 +0200

    added a new find method (which we might need if we use application-side joining for favorites in the elastic-job-ad-search)

--- domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/favouriteitem/FavouriteItemRepository.java
+++ domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/favouriteitem/FavouriteItemRepository.java
@@ -7,12 +7,19 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Transactional(propagation = Propagation.MANDATORY)
-public interface FavouriteItemRepository  extends JpaRepository<FavouriteItem, FavouriteItemId> {
+public interface FavouriteItemRepository extends JpaRepository<FavouriteItem, FavouriteItemId> {
 
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Query("select i from FavouriteItem i where i.jobAdvertisementId = :jobAdvertisementId and i.ownerId = :ownerId")
-    Optional<FavouriteItem> findByJobAdvertisementIdAndOwnerId(@Param("jobAdvertisementId") JobAdvertisementId jobAdvertisementId, @Param("ownerId")  String ownerId);
+    Optional<FavouriteItem> findByJobAdvertisementIdAndOwnerId(@Param("jobAdvertisementId") JobAdvertisementId jobAdvertisementId, @Param("ownerId") String ownerId);
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Query("select i from FavouriteItem i where i.jobAdvertisementId in :jobAdvertisementIds and i.ownerId = :ownerId")
+    List<FavouriteItem> findByJobAdvertisementIdsAndOwnerId(@Param("jobAdvertisementIds") Set<JobAdvertisementId> jobAdvertisementIds, @Param("ownerId") String ownerId);
 
 }
