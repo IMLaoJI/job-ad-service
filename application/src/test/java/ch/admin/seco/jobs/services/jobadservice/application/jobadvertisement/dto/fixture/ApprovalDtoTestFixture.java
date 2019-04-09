1e980524e56d99ffbda26ac998e58a5719e44efc
Emina Stricak <eminastricak@gmail.com>
2019-04-09 16:02:47 +0200

    DF 850 - Avam JobAd, wrong JobCenterId - unused import

--- application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/dto/fixture/ApprovalDtoTestFixture.java
+++ application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/dto/fixture/ApprovalDtoTestFixture.java
@@ -1,15 +1,14 @@
 package ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture;
 
-import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationServiceForAvamTest.JOB_CENTER_ID;
-import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationServiceForAvamTest.STELLENNUMMER_AVAM;
-import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture.UpdateJobAdvertisementFromAvamDtoTestFixture.testUpdateJobAdvertisementFromAvamDto;
-import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.fixture.JobAdvertisementIdFixture.job01;
-
-import java.time.LocalDate;
-
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.update.ApprovalDto;
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.update.UpdateJobAdvertisementFromAvamDto;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.JobAdvertisement;
+
+import java.time.LocalDate;
+
+import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationServiceForAvamTest.JOB_CENTER_ID;
+import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationServiceForAvamTest.STELLENNUMMER_AVAM;
+import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture.UpdateJobAdvertisementFromAvamDtoTestFixture.testUpdateJobAdvertisementFromAvamDto;
 
 
 public class ApprovalDtoTestFixture {
