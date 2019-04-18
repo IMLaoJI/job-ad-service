668ecfffc97c8e92185235ca95066743283ffe89
Patrick Dobler <patrick.dobler@gmail.com>
2019-04-18 17:12:12 +0200

    cleanup: 1) a infrastructure module must not depend on another infrastructure module to avoid a big-ball-of mud 2. the applications avam-sink, avam-source, x28-job-export and x28-job-import only depend on the DTO's or Entities of the Application/Core and thus should not depend on the infrastructure

--- infrastructure-messagebroker/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/TestConfig.java
+++ infrastructure-messagebroker/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/TestConfig.java
@@ -1,38 +1,51 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.avam;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.NullChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
 import ch.admin.seco.jobs.services.jobadservice.application.JobCenterService;
 import ch.admin.seco.jobs.services.jobadservice.application.MailSenderService;
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationService;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.JobAdvertisementRepository;
 import ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.MessageBrokerChannels;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class TestConfig {
 
-    @MockBean
-    MailSenderService mailSenderService;
+	@MockBean
+	MailSenderService mailSenderService;
 
-    @MockBean
-    JobAdvertisementApplicationService jobAdvertisementApplicationService;
+	@MockBean
+	JobAdvertisementApplicationService jobAdvertisementApplicationService;
 
-    @MockBean
-    JobCenterService jobCenterService;
+	@MockBean
+	JobCenterService jobCenterService;
 
-    @MockBean
-    MessageBrokerChannels messageBrokerChannels;
+	@MockBean
+	JobAdvertisementRepository jobAdvertisementRepository;
 
-    @MockBean
-    JobAdvertisementRepository jobAdvertisementRepository;
+	@Bean
+	MessageBrokerChannels messageBrokerChannels() {
+		return new MessageBrokerChannels() {
+			@Override
+			public SubscribableChannel jobAdIntActionChannel() {
+				return new DirectChannel();
+			}
 
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-        source.setBasenames("mails/i18n/mail-messages");
-        return source;
-    }
+			@Override
+			public MessageChannel jobAdIntEventChannel() {
+				return new NullChannel();
+			}
 
+			@Override
+			public MessageChannel jobAdEventChannel() {
+				return new NullChannel();
+			}
+		};
+	}
 }
