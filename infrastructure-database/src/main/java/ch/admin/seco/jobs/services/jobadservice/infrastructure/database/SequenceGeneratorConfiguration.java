ac291b628000c727a5e5e93cce64023b7b19b8a1
Patrick Dobler <patrick.dobler@gmail.com>
2019-04-18 16:38:43 +0200

    cleanup

--- infrastructure-database/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/database/SequenceGeneratorConfiguration.java
+++ infrastructure-database/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/database/SequenceGeneratorConfiguration.java
@@ -1,6 +1,7 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.database;
 
-import org.apache.commons.lang3.NotImplementedException;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -12,36 +13,34 @@ import org.springframework.jdbc.support.incrementer.DataFieldMaxValueIncrementer
 import org.springframework.jdbc.support.incrementer.H2SequenceMaxValueIncrementer;
 import org.springframework.jdbc.support.incrementer.PostgresSequenceMaxValueIncrementer;
 
-import javax.sql.DataSource;
-
 @Configuration
 @ConditionalOnClass({DataSource.class})
 @AutoConfigureAfter(DataSourceAutoConfiguration.class)
 public class SequenceGeneratorConfiguration {
-    private final DataSource dataSource;
-    private final DataSourceProperties dataSourceProperties;
-    private final String stellennummerEgovGeneratorSequenceName;
+	private final DataSource dataSource;
+	private final DataSourceProperties dataSourceProperties;
+	private final String stellennummerEgovGeneratorSequenceName;
 
-    public SequenceGeneratorConfiguration(DataSource dataSource,
-                                          DataSourceProperties dataSourceProperties,
-                                          @Value("${jobAdvertisement.stellennummerEgovGeneratorSequenceName:STELLEN_NUM_EGOV_SEQ}")
-                                                  String egovNumberGeneratorSequenceName) {
-        this.dataSource = dataSource;
-        this.dataSourceProperties = dataSourceProperties;
-        this.stellennummerEgovGeneratorSequenceName = egovNumberGeneratorSequenceName;
-    }
+	public SequenceGeneratorConfiguration(DataSource dataSource,
+			DataSourceProperties dataSourceProperties,
+			@Value("${jobAdvertisement.stellennummerEgovGeneratorSequenceName:STELLEN_NUM_EGOV_SEQ}")
+					String egovNumberGeneratorSequenceName) {
+		this.dataSource = dataSource;
+		this.dataSourceProperties = dataSourceProperties;
+		this.stellennummerEgovGeneratorSequenceName = egovNumberGeneratorSequenceName;
+	}
 
-    @Bean
-    public DataFieldMaxValueIncrementer stellennummerEgovGenerator() {
-        final String driverClassName = this.dataSourceProperties.determineDriverClassName();
+	@Bean
+	public DataFieldMaxValueIncrementer stellennummerEgovGenerator() {
+		final String driverClassName = this.dataSourceProperties.determineDriverClassName();
 
-        if (driverClassName.toUpperCase().contains("H2")) {
-            return new H2SequenceMaxValueIncrementer(dataSource, stellennummerEgovGeneratorSequenceName);
-        } else if (driverClassName.toUpperCase().contains("POSTGRESQL")) {
-            return new PostgresSequenceMaxValueIncrementer(dataSource, stellennummerEgovGeneratorSequenceName);
-        } else {
-            throw new NotImplementedException(String.format("Implementation is not found for: %s", driverClassName));
-        }
-    }
+		if (driverClassName.toUpperCase().contains("H2")) {
+			return new H2SequenceMaxValueIncrementer(dataSource, stellennummerEgovGeneratorSequenceName);
+		} else if (driverClassName.toUpperCase().contains("POSTGRESQL")) {
+			return new PostgresSequenceMaxValueIncrementer(dataSource, stellennummerEgovGeneratorSequenceName);
+		} else {
+			throw new UnsupportedOperationException(String.format("Implementation is not found for: %s", driverClassName));
+		}
+	}
 
 }
