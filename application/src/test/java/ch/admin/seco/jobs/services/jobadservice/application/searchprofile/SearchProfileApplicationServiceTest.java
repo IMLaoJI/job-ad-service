2bd7ff75756ec575a7ff17c97e92d5fb3a4910e4
Emina Stricak <eminastricak@gmail.com>
2019-05-03 15:16:39 +0200

    DF 948 job search-profiles - test files

---
+++ application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/searchprofile/SearchProfileApplicationServiceTest.java
@@ -0,0 +1,13 @@
+package ch.admin.seco.jobs.services.jobadservice.application.searchprofile;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Transactional
+public class SearchProfileApplicationServiceTest {
+    // TODO
+}
