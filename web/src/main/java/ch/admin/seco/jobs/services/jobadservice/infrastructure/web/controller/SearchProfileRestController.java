2ba2a82716da3926fb650786eda1aae70844ee6a
Martin Dvoracek <martin.dvoracek@mimacom.com>
2019-05-06 17:37:42 +0200

    DF-948 ControllerTest implemented

--- web/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/controller/SearchProfileRestController.java
+++ web/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/controller/SearchProfileRestController.java
@@ -15,7 +15,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -46,11 +55,11 @@ public class SearchProfileRestController {
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public SearchProfileDto updateSearchProfile(@PathVariable SearchProfileId id, @RequestBody UpdateSearchProfileResouce updateSearchProfileResouce)
+    public SearchProfileDto updateSearchProfile(@PathVariable SearchProfileId id, @RequestBody UpdateSearchProfileResource updateSearchProfileResource)
             throws SearchProfileNotExitsException {
 
         UpdateSearchProfileDto updateSearchProfileDto = new UpdateSearchProfileDto(
-                id, updateSearchProfileResouce.name, SearchFilterDto.toDto(updateSearchProfileResouce.searchFilter)
+                id, updateSearchProfileResource.name, SearchFilterDto.toDto(updateSearchProfileResource.searchFilter)
         );
         return this.searchProfileApplicationService.updateSearchProfile(updateSearchProfileDto);
     }
@@ -88,7 +97,7 @@ public class SearchProfileRestController {
         public SearchFilter searchFilter;
     }
 
-    static class UpdateSearchProfileResouce {
+    static class UpdateSearchProfileResource {
         @NotBlank
         @Size(max = 100)
         public String name;
--- web/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/controller/SearchProfileRestControllerIntTest.java
+++ web/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/web/controller/SearchProfileRestControllerIntTest.java
@@ -1,7 +1,12 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.web.controller;
 
+import ch.admin.seco.jobs.services.jobadservice.domain.searchprofile.SearchProfile;
+import ch.admin.seco.jobs.services.jobadservice.domain.searchprofile.SearchProfileId;
 import ch.admin.seco.jobs.services.jobadservice.domain.searchprofile.SearchProfileRepository;
+import ch.admin.seco.jobs.services.jobadservice.domain.searchprofile.fixture.SearchProfileFixture;
+import ch.admin.seco.jobs.services.jobadservice.domain.searchprofile.fixture.SearchProfileIdFixture;
 import ch.admin.seco.jobs.services.jobadservice.infrastructure.web.TestUtil;
+import org.codehaus.jettison.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +18,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,8 +45,62 @@ public class SearchProfileRestControllerIntTest {
 
     @Test
     @WithJobSeeker
-    public void testCreate() {
-        // implement tests
+    public void testCreateSearchProfile() throws Exception {
+        // given
+        SearchProfileRestController.CreateSearchProfileResource createSearchProfileResource = new SearchProfileRestController.CreateSearchProfileResource();
+        SearchProfileIdFixture searchProfileId = SearchProfileIdFixture.search_profile_01;
+        SearchProfile searchProfile = SearchProfileFixture.testSearchProfile(searchProfileId.id());
+        createSearchProfileResource.name = searchProfile.getName();
+        createSearchProfileResource.userOwnerId = searchProfile.getOwnerUserId();
+        createSearchProfileResource.searchFilter = searchProfile.getSearchFilter();
+
+        // when
+        ResultActions post = post(createSearchProfileResource, URL);
+        post.andExpect(status().isCreated());
+
+        String contentAsString = post.andReturn().getResponse().getContentAsString();
+        JSONArray ja = new JSONArray("[" + contentAsString + "]");
+        String id = ja.getJSONObject(0).getString("id");
+
+        // then
+        assertThat(this.searchProfileRepository.findById(new SearchProfileId(id))).isPresent();
+    }
+
+
+    @Test
+    @WithJobSeeker
+    public void testUpdateSearchProfile() throws Exception {
+        // given
+        SearchProfileRestController.CreateSearchProfileResource createSearchProfileResource = new SearchProfileRestController.CreateSearchProfileResource();
+        SearchProfileIdFixture searchProfileId = SearchProfileIdFixture.search_profile_01;
+        SearchProfile testSearchProfile = SearchProfileFixture.testSearchProfile(searchProfileId.id());
+        createSearchProfileResource.name = testSearchProfile.getName();
+        createSearchProfileResource.userOwnerId = testSearchProfile.getOwnerUserId();
+        createSearchProfileResource.searchFilter = testSearchProfile.getSearchFilter();
+        ResultActions post = post(createSearchProfileResource, URL);
+        post.andExpect(status().isCreated());
+
+        SearchProfileRestController.UpdateSearchProfileResource updateSearchProfileResource = new SearchProfileRestController.UpdateSearchProfileResource();
+        String adjustedName = "Adjusted Name";
+        updateSearchProfileResource.name = adjustedName;
+        updateSearchProfileResource.searchFilter = testSearchProfile.getSearchFilter();
+
+        // when
+        ResultActions put = this.mockMvc.perform(
+                MockMvcRequestBuilders.put(URL + "/" + searchProfileId.id().getValue())
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(updateSearchProfileResource)));
+        put.andExpect(status().isOk());
+
+
+        String contentAsString = post.andReturn().getResponse().getContentAsString();
+        JSONArray ja = new JSONArray("[" + contentAsString + "]");
+        String id = ja.getJSONObject(0).getString("id");
+
+        // then
+        Optional<SearchProfile> searchProfile = this.searchProfileRepository.findById(testSearchProfile.getId());
+        assertThat(searchProfile).isPresent();
+        assertThat(searchProfile.get().getName()).isEqualTo(adjustedName);
     }
 
     private ResultActions post(Object request, String urlTemplate) throws Exception {
