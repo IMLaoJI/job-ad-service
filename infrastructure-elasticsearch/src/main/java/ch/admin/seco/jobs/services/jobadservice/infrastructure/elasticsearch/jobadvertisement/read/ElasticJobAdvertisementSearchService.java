1473fc5b191e368ddfa6094ac8fc9db492b9b279
fago <fabian.gotzen@gmail.com>
2019-05-27 16:59:20 +0200

    DF-1008 - adjust parameters after tests

--- infrastructure-elasticsearch/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/elasticsearch/jobadvertisement/read/ElasticJobAdvertisementSearchService.java
+++ infrastructure-elasticsearch/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/elasticsearch/jobadvertisement/read/ElasticJobAdvertisementSearchService.java
@@ -82,9 +82,9 @@ public class ElasticJobAdvertisementSearchService implements JobAdvertisementSea
 
 
 	private static final String PATH_CREATED_TIME = "jobAdvertisement.createdTime";
-	private static final String SCALE_IN_KM = "30km";
-	private static final String OFFSET_IN_KM = "10km";
-	private static final double DECAY_RATE = 0.8;
+	private static final String SCALE_IN_KM = "150km";
+	private static final String OFFSET_IN_KM = "30km";
+	private static final double DECAY_RATE = 0.5;
 
 	private static Logger LOG = LoggerFactory.getLogger(ElasticJobAdvertisementSearchService.class);
 
