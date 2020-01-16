1f44d1bee04f0ba068112191fc28b3b5cb9cdb41
Marton Biro <marton.biro@mimacom.com>
2020-01-16 22:16:12 +0100

    DF-1610: Test openshift-client-plugin

--- openshift/jobadservice-app/jenkinsfile.groovy
+++ openshift/jobadservice-app/jenkinsfile.groovy
@@ -92,5 +92,13 @@ pipeline {
                 )
             }
         }
+
+        stage('Build Docker'){
+            steps {
+                openshift.withCluster() { // Use "default" cluster or fallback to OpenShift cluster detection
+                    echo "Hello from the project running Jenkins: ${openshift.project()}"
+                }
+            }
+        }
     }
 }
