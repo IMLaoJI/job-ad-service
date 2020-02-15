6bce667eb56726b538d4e748bad6c9135165a489
Vinh Nguyen <vinh.nguyen@mimacom.com>
2020-02-15 18:04:58 +0100

    DF-2083 improve docker build
    switch back to master branch of templates

--- openshift/job-ad-service/jenkinsfile.groovy
+++ openshift/job-ad-service/jenkinsfile.groovy
@@ -95,7 +95,7 @@ pipeline {
                 checkout changelog: false,
                         poll: false,
                         scm: [$class                           : 'GitSCM',
-                              branches                         : [[name: '*/feature/improve-docker-build']],
+                              branches                         : [[name: '*/master']],
                               doGenerateSubmoduleConfigurations: false,
                               extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'ocp-commons']],
                               submoduleCfg                     : [],
