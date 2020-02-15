f61c9ba2a92fb25ccf92329afdd9ce3aee8eb156
Vinh Nguyen <vinh.nguyen@mimacom.com>
2020-02-15 17:41:14 +0100

    DF-2083 improve docker build
    use new templates

--- openshift/job-ad-service/jenkinsfile.groovy
+++ openshift/job-ad-service/jenkinsfile.groovy
@@ -95,7 +95,7 @@ pipeline {
                 checkout changelog: false,
                         poll: false,
                         scm: [$class                           : 'GitSCM',
-                              branches                         : [[name: '*/master']],
+                              branches                         : [[name: '*/feature/improve-docker-build']],
                               doGenerateSubmoduleConfigurations: false,
                               extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'ocp-commons']],
                               submoduleCfg                     : [],
