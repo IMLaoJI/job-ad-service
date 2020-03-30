43a35c3b3be2018492e1c8bd76ac05c93a7b860d
Marton Biro <marton.biro@mimacom.com>
2020-03-30 20:24:44 +0200

    Feature/df 2291 use shared pipeline scripts (#445)
    * DF-2039: Add create-task script
    * DF-2291: Override application folder in the appJobAdService docker build

--- openshift/job-ad-service/jenkinsfile.groovy
+++ openshift/job-ad-service/jenkinsfile.groovy
@@ -1,245 +1,45 @@
-pipeline {
-    agent { label 'maven' }
+@Library('alv-ch-shared-library') _
 
-    options {
-        buildDiscarder(logRotator(numToKeepStr: '5'))
-        timeout(time: 10, unit: 'MINUTES')
-    }
+node('maven') {
 
-    environment {
-        DEFAULT_JVM_OPTS = '-Dhttp.proxyHost=l98fppx1.admin.arbeitslosenkasse.ch -Dhttp.proxyPort=8080 -Dhttps.proxyHost=l98fppx1.admin.arbeitslosenkasse.ch -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts="*.admin.arbeitslosenkasse.ch|172.27.97.10|172.27.97.11|172.27.97.12|172.30.0.1"'
-        JAVA_TOOL_OPTIONS = "$JAVA_TOOL_OPTIONS $DEFAULT_JVM_OPTS"
-        SERVER_URL = "https://alvch.jfrog.io/alvch"
-        CREDENTIALS = "artifactory-deploy"
-        ARTIFACTORY_SERVER = "alv-ch"
-        MAVEN_HOME = "/opt/rh/rh-maven35/root/usr/share/xmvn"
-        SONAR_LOGIN = credentials('SONAR_TOKEN')
-        SONAR_SERVER = "${env.SONAR_HOST_URL}"
-        NAMESPACE_NAME = "jobroom-dev"
-        PROJECT_NAME = "job-ad-service"
-        ARTIFACT_VERSION = "${new Date().format("YYYY-MM-dd")}.${BUILD_NUMBER}"
-    }
+    withEnv(initEnv()) {
 
-    stages {
+        checkoutScm()
 
-        stage('Initialize') {
-            steps {
-                sh '''
-                  printenv
-                  echo "...Building on branch: ${GIT_BRANCH}"
-                  echo "PATH = ${PATH}"
-                  echo "MAVEN_HOME = ${MAVEN_HOME}"
-                  echo ARTIFACT_VERSION = ${ARTIFACT_VERSION}
-              '''
-            }
-        }
+        mavenBuild()
 
-        stage('Artifactory configuration') {
-            steps {
-                rtServer(
-                        id: ARTIFACTORY_SERVER,
-                        url: SERVER_URL,
-                        credentialsId: CREDENTIALS
-                )
+        sonarQube([projectKey: 'job-ad-service'])
 
-                rtMavenDeployer(
-                        id: "MAVEN_DEPLOYER",
-                        serverId: ARTIFACTORY_SERVER,
-                        releaseRepo: "libs-releases-local",
-                        snapshotRepo: "libs-snapshots-local"
-                )
+        updateOpenshiftTemplates()
 
-                rtMavenResolver(
-                        id: "MAVEN_RESOLVER",
-                        serverId: ARTIFACTORY_SERVER,
-                        releaseRepo: "libs-releases-ocp",
-                        snapshotRepo: "libs-snapshots"
-                )
-
-                rtMavenResolver(
-                        id: "MAVEN_RESOLVER",
-                        serverId: ARTIFACTORY_SERVER,
-                        releaseRepo: "plugin-releases-ocp",
-                        snapshotRepo: "plugins-snapshots"
-                )
-            }
-        }
-
-        stage('Exec Maven') {
-            steps {
-                rtMavenRun(
-                        pom: 'pom.xml',
-                        goals: 'initialize -DnewVersion=${ARTIFACT_VERSION}',
-                        resolverId: "MAVEN_RESOLVER"
-                )
-                rtMavenRun(
-                        pom: 'pom.xml',
-                        goals: 'package -DskipTests -DskipITs=true',
-                        deployerId: "MAVEN_DEPLOYER",
-                        resolverId: "MAVEN_RESOLVER"
-                )
-
-                rtPublishBuildInfo(
-                        serverId: ARTIFACTORY_SERVER
-                )
-
-                // copy resources for docker build
-                sh '''
-                    cp "${WORKSPACE}/web/target/app-job-ad-service.jar" "${WORKSPACE}/openshift/job-ad-service/app-job-ad-service/"
-                    cp "${WORKSPACE}/app-external-job-ad-export-task/target/app-external-job-ad-export-task.jar" "${WORKSPACE}/openshift/job-ad-service/app-external-job-ad-export-task/"
-                    cp "${WORKSPACE}/app-external-job-ad-import-task/target/app-external-job-ad-import-task.jar" "${WORKSPACE}/openshift/job-ad-service/app-external-job-ad-import-task/"
-                '''
-            }
-        }
-
-        stage('Update templates') {
-            steps {
-                checkout changelog: false,
-                        poll: false,
-                        scm: [$class                           : 'GitSCM',
-                              branches                         : [[name: '*/master']],
-                              doGenerateSubmoduleConfigurations: false,
-                              extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'ocp-commons']],
-                              submoduleCfg                     : [],
-                              userRemoteConfigs                : [[credentialsId: 'cicd-alvchbot-at-github',
-                                                                   url          : 'https://github.com/alv-ch/ocp-commons.git']]]
-                script {
-                    openshift.withCluster() {
-                        openshift.withProject('jobroom-dev') {
-                            def templateNames = [
-                                    'microservice-app-build-config-docker-template',
-                                    'batch-app-build-config-docker-template'
-                            ]
-
-                            templateNames.each {
-                                openshift.apply(readFile("${WORKSPACE}/ocp-commons/templates/${it}.yml"))
-                            }
-                        }
-                    }
+        parallel([
+                appJobAdService           : {
+                    microserviceDockerBuild([project: 'job-ad-service', application: 'app-job-ad-service', applicationFolderOverride: 'web'])
+                },
+                appExternalJobAdExportTask: {
+                    batchApplicationDockerBuild([project: 'job-ad-service', application: 'app-external-job-ad-export-task'])
+                },
+                appExternalJobAdImportTask: {
+                    batchApplicationDockerBuild([project: 'job-ad-service', application: 'app-external-job-ad-import-task'])
                 }
-            }
-        }
+        ])
 
-        stage('SonarQube') {
-            // our SonarQube has only master
-            when {
-                branch 'master'
-            }
+        scdfDeployment([
+                application  : 'app-external-job-ad-export-task',
+                version      : ARTIFACT_VERSION,
+                cron         : '40 18 * * *',
+                taskArguments: ['--spring.cloud.config.name': 'app-prisme-job-ad-export']
+        ])
 
-            steps {
-                rtMavenRun(
-                        pom: 'pom.xml',
-                        goals: 'sonar:sonar -Dsonar.projectKey=job-ad-service -Dsonar.host.url="$SONAR_SERVER" -Dsonar.login=$SONAR_LOGIN',
-                        resolverId: "MAVEN_RESOLVER"
-                )
-            }
-        }
+        scdfDeployment([
+                application  : 'app-external-job-ad-import-task',
+                version      : ARTIFACT_VERSION,
+                cron         : '0 2 * * *',
+                taskArguments: ['--spring.cloud.config.name': 'app-prisme-job-ad-import']
+        ])
 
+        //The application value is referring the application in the deployment config
+        openshiftDeployment([application: 'app-job-ad-service'])
 
-        stage('Docker Builds') {
-            when {
-                branch 'feature/openshift'
-            }
-
-            parallel {
-                stage('Docker Build job-ad-service in jobroom-dev') {
-                    steps {
-                        script {
-                            openshift.withCluster() {
-                                openshift.withProject('jobroom-dev') {
-                                    def microserviceAppBuildConfigDockerTemplate = openshift.selector("template", "microservice-app-build-config-docker-template").object()
-                                    openshift.apply(openshift.process(microserviceAppBuildConfigDockerTemplate, [
-                                            "-p", "NAMESPACE=${NAMESPACE_NAME}",
-                                            "-p", "MICROSERVICE_PROJECT_NAME=${PROJECT_NAME}",
-                                            "-p", "APPLICATION_NAME=app-job-ad-service",
-                                    ]))
-
-                                    def build = openshift.selector('bc', 'app-job-ad-service-docker').startBuild("--from-dir ./openshift/job-ad-service/app-job-ad-service")
-                                    result = build.logs('-f')
-
-                                    if (result.status == 0) {
-                                        return true
-                                    }
-
-                                    return false
-                                }
-                            }
-                        }
-                    }
-                }
-
-                stage('Docker Build app-external-job-ad-export-task in jobroom-dev') {
-                    steps {
-                        script {
-                            openshift.withCluster() {
-                                openshift.withProject('jobroom-dev') {
-                                    def batchAppBuildConfigDockerTemplate = openshift.selector("template", "batch-app-build-config-docker-template").object()
-                                    openshift.apply(openshift.process(batchAppBuildConfigDockerTemplate, [
-                                            "-p", "NAMESPACE=${NAMESPACE_NAME}",
-                                            "-p", "MICROSERVICE_PROJECT_NAME=${PROJECT_NAME}",
-                                            "-p", "APPLICATION_NAME=app-external-job-ad-export-task",
-                                            "-p", "IMAGE_LABEL=${ARTIFACT_VERSION}"
-                                    ]))
-
-                                    def build = openshift.selector('bc', 'app-external-job-ad-export-task-docker').startBuild("--from-dir ./openshift/job-ad-service/app-external-job-ad-export-task")
-                                    result = build.logs('-f')
-
-                                    if (result.status == 0) {
-                                        return true
-                                    }
-
-                                    return false
-                                }
-                            }
-                        }
-                    }
-                }
-
-                stage('Docker Build app-external-job-ad-import-task in jobroom-dev') {
-                    steps {
-                        script {
-                            openshift.withCluster() {
-                                openshift.withProject('jobroom-dev') {
-                                    def batchAppBuildConfigDockerTemplate = openshift.selector("template", "batch-app-build-config-docker-template").object()
-                                    openshift.apply(openshift.process(batchAppBuildConfigDockerTemplate, [
-                                            "-p", "NAMESPACE=${NAMESPACE_NAME}",
-                                            "-p", "MICROSERVICE_PROJECT_NAME=${PROJECT_NAME}",
-                                            "-p", "APPLICATION_NAME=app-external-job-ad-import-task",
-                                            "-p", "IMAGE_LABEL=${ARTIFACT_VERSION}"
-                                    ]))
-
-                                    def build = openshift.selector('bc', 'app-external-job-ad-import-task-docker').startBuild("--from-dir ./openshift/job-ad-service/app-external-job-ad-import-task")
-                                    result = build.logs('-f')
-
-                                    if (result.status == 0) {
-                                        return true
-                                    }
-
-                                    return false
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        stage('Deploy to jobroom-dev') {
-            when {
-                branch 'feature/openshift'
-            }
-
-            steps {
-                script {
-                    openshift.withCluster() {
-                        openshift.withProject("${NAMESPACE_NAME}") {
-                            def appJobAdServiceDC = openshift.selector('dc', 'app-job-ad-service')
-                            appJobAdServiceDC.rollout().latest()
-                            appJobAdServiceDC.rollout().status()
-                        }
-                    }
-                }
-            }
-        }
     }
 }
