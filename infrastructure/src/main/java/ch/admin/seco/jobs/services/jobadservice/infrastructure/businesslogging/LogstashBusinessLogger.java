c025dde750e5dde2abfe7b32d180b05ff9a2d4b0
emina-str <45841069+emina-str@users.noreply.github.com>
2020-04-01 10:29:03 +0200

    DF-2146 : remove comma from authorities list toString() method (#444)

--- infrastructure/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/businesslogging/LogstashBusinessLogger.java
+++ infrastructure/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/businesslogging/LogstashBusinessLogger.java
@@ -32,6 +32,6 @@ public class LogstashBusinessLogger implements BusinessLogger {
 	}
 
 	private String extractAuthorities(CurrentUser currentUser) {
-		return currentUser != null ? String.join(", ", currentUser.getAuthorities()) : Strings.EMPTY;
+		return currentUser != null ? String.join(" ", currentUser.getAuthorities()) : Strings.EMPTY;
 	}
 }
