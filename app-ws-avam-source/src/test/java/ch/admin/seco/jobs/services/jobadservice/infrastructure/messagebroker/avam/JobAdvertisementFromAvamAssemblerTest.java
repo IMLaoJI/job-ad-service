74d92b2af5f478beec6e2fd5790bcb6f2c9fce1b
Marius Gabor <marius.gabor@seco.admin.ch>
2020-04-24 08:26:56 +0200

    DF-2418: More unit tests for immediately flag added.

--- app-ws-avam-source/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/JobAdvertisementFromAvamAssemblerTest.java
+++ app-ws-avam-source/src/test/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/JobAdvertisementFromAvamAssemblerTest.java
@@ -8,11 +8,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JobAdvertisementFromAvamAssemblerTest {
     @Test
-    public void shouldNotBeImmediately() {
+    public void shouldNotBeImmediatelyWhenStellenantrittIsNull() {
         // given
         JobAdvertisementFromAvamAssembler assembler = new JobAdvertisementFromAvamAssembler();
         WSOsteEgov avamJobAd = new WSOsteEgov();
         avamJobAd.setStellenantritt(null);
+        // when
+        AvamCreateJobAdvertisementDto jobAdvertisementDto = assembler.createCreateJobAdvertisementAvamDto(avamJobAd);
+        // then
+        assertThat(jobAdvertisementDto.getEmployment().isImmediately()).isFalse();
+    }
+
+    @Test
+    public void shouldNotBeImmediatelyWhenStellenantrittIsNotNull() {
+        // given
+        JobAdvertisementFromAvamAssembler assembler = new JobAdvertisementFromAvamAssembler();
+        WSOsteEgov avamJobAd = new WSOsteEgov();
+        avamJobAd.setStellenantritt("2020-04-24-00.00.00.0");
+        // when
+        AvamCreateJobAdvertisementDto jobAdvertisementDto = assembler.createCreateJobAdvertisementAvamDto(avamJobAd);
+        // then
+        assertThat(jobAdvertisementDto.getEmployment().isImmediately()).isFalse();
+    }
+
+    @Test
+    public void shouldBeImmediatelyWhenSetInAvam() {
+        // given
+        JobAdvertisementFromAvamAssembler assembler = new JobAdvertisementFromAvamAssembler();
+        WSOsteEgov avamJobAd = new WSOsteEgov();
+        avamJobAd.setAbSofort(Boolean.TRUE);
+        // when
+        AvamCreateJobAdvertisementDto jobAdvertisementDto = assembler.createCreateJobAdvertisementAvamDto(avamJobAd);
+        // then
+        assertThat(jobAdvertisementDto.getEmployment().isImmediately()).isTrue();
+    }
+
+    @Test
+    public void shouldNotBeImmediatelyWhenSetInAvamWithFalse() {
+        // given
+        JobAdvertisementFromAvamAssembler assembler = new JobAdvertisementFromAvamAssembler();
+        WSOsteEgov avamJobAd = new WSOsteEgov();
+        avamJobAd.setAbSofort(Boolean.FALSE);
+        // when
+        AvamCreateJobAdvertisementDto jobAdvertisementDto = assembler.createCreateJobAdvertisementAvamDto(avamJobAd);
+        // then
+        assertThat(jobAdvertisementDto.getEmployment().isImmediately()).isFalse();
+    }
+
+    @Test
+    public void shouldNotBeImmediatelyWhenNotSetInAvam() {
+        // given
+        JobAdvertisementFromAvamAssembler assembler = new JobAdvertisementFromAvamAssembler();
+        WSOsteEgov avamJobAd = new WSOsteEgov();
+        avamJobAd.setAbSofort(null);
         // when
         AvamCreateJobAdvertisementDto jobAdvertisementDto = assembler.createCreateJobAdvertisementAvamDto(avamJobAd);
         // then
