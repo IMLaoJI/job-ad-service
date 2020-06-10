6dae165d9f30a1285e33a40d55c7b46b74a2180e
fago <fabian.gotzen@mimacom.com>
2020-06-10 15:36:49 +0200

    DF-219 - fix test

--- application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/dto/fixture/CreateJobAdvertisementFromAvamDtoTestFixture.java
+++ application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/dto/fixture/CreateJobAdvertisementFromAvamDtoTestFixture.java
@@ -1,37 +1,14 @@
 package ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture;
 
-import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationServiceForAvamTest.STELLENNUMMER_AVAM;
-import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture.PublicationDtoTestFixture.testPublicationDto;
-import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture.PublicationDtoTestFixture.testPublicationDtoWithCompanyAnonymous;
-import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.LanguageLevel.PROFICIENT;
-import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.WorkExperience.MORE_THAN_1_YEAR;
-import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.fixture.CompanyFixture.testCompany;
-import static java.time.LocalDate.now;
-import static java.util.Collections.singletonList;
-
-import java.time.LocalDate;
-
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.JobDescriptionDto;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.*;
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.create.CreateJobAdvertisementDto;
-import org.assertj.core.util.Sets;
-
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.AddressDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.ApplyChannelDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.CompanyDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.ContactDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.EmploymentDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.LanguageSkillDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.OccupationDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.PublicContactDto;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.PublicationDto;
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.create.CreateLocationDto;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.Company;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.Qualification;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.Salutation;
-import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.WorkForm;
+import org.assertj.core.util.Sets;
 
 import java.time.LocalDate;
-import java.util.Collections;
 
 import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationServiceForAvamTest.STELLENNUMMER_AVAM;
 import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.fixture.PublicationDtoTestFixture.testPublicationDto;
@@ -39,8 +16,7 @@ import static ch.admin.seco.jobs.services.jobadservice.application.jobadvertisem
 import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.LanguageLevel.PROFICIENT;
 import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.WorkExperience.MORE_THAN_1_YEAR;
 import static ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.fixture.CompanyFixture.testCompany;
-import static com.google.common.collect.Sets.immutableEnumSet;
-import static java.time.LocalDate.now;
+import static java.util.Collections.singletonList;
 
 public class CreateJobAdvertisementFromAvamDtoTestFixture {
 
@@ -60,6 +36,7 @@ public class CreateJobAdvertisementFromAvamDtoTestFixture {
                 .setReportingObligation(true)
                 .setReportingObligationEndDate(LocalDate.of(2018, 1, 1))
                 .setJobCenterCode("jobCenter")
+                .setJobCenterUserId("Markus Meier")
                 .setApprovalDate(LocalDate.of(2017, 12, 26))
                 .setEmployment(new EmploymentDto()
                         .setStartDate(LocalDate.of(2018, 1, 1))
