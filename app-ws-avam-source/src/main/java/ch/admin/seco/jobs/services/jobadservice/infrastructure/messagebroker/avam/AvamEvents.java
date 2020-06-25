3c91db7b06aa567b52bf44a8cf990f326a76f768
fago <fabian.gotzen@mimacom.com>
2020-06-25 10:07:10 +0200

    DF-219 - add AvamEvents, adjust guessing logic

---
+++ app-ws-avam-source/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamEvents.java
@@ -0,0 +1,6 @@
+package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.avam;
+
+public enum AvamEvents {
+	AKTIVIERT, ABGELEHNT, MUTIERT,
+	INAKTIVIERT, REAKTIVIERT, ABGEMELDET, GELOESCHT;
+}
