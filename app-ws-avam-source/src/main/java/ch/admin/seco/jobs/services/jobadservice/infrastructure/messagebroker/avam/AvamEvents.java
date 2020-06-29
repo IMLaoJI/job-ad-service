5ecd75e4c917bc2a5749b27e41b1794d92cb31c0
fago <fabian.gotzen@mimacom.com>
2020-06-29 11:46:36 +0200

    DF-1193 - refactor, extract AvamEvents

---
+++ app-ws-avam-source/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamEvents.java
@@ -0,0 +1,6 @@
+package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.avam;
+
+public enum AvamEvents {
+	AKTIVIERT, ABGEMELDET, MUTIERT, ABGELEHNT, INAKTIVIERT, GELOESCHT, REAKTIVIERT;
+}
+
